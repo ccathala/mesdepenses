44bf3c0ed8eac00864909bda6a3d6aec29ec7d9e
suricath <89075025+suricath@users.noreply.github.com>
2022-07-08 09:07:57 +0200

    Create spring project

---
+++ src/main/java/com/ccathala/mesdepenses/MesdepensesApplication.java
@@ -0,0 +1,13 @@
+package com.ccathala.mesdepenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesdepensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesdepensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccathala/mesdepenses/MesdepensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccathala.mesdepenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesdepensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
